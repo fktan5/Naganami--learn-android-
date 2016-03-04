deacbce1558168b7cdbd4de73cbd1c224288a022
fktan5 <email@fktan5.net>
2016-03-04 18:11:59 +0900

    SharedPrefsの検証

--- Hoge001/app/src/main/java/net/fktan5/hoge001/MainActivity.java
+++ Hoge001/app/src/main/java/net/fktan5/hoge001/MainActivity.java
@@ -1,9 +1,11 @@
 package net.fktan5.hoge001;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 
 import butterknife.Bind;
@@ -11,6 +13,8 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static String KEY = "key";
 
     @OnClick(R.id.button) void nextActivity(){
         Intent intent = new Intent(this, SecondActivity.class);
@@ -28,5 +32,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        //SharedPreferenceの実験
+        float value = 1455500647.680438f;
+        double value2 = 1455500647.680438;
+        Log.d(KEY, "value : " + value);
+        Log.d(KEY, "value2 : " + value2);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("DemoApp", MODE_PRIVATE);
+        sharedPreferences.edit().putFloat(KEY, value).apply();
     }
 }
